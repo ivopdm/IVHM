a704634fdb367a0f162bf3ccc2342d981d2750f0
ivopdm <ivopdm@gmail.com>
2017-08-15 12:57:15 -0300

    Classe para guardar Proposta
    Classe para guardar uma rota (conjunto de voos) e o preco da rota para
    uma dada proposta

---
+++ IVHM/src/commons/Proposal.java
@@ -0,0 +1,24 @@
+package commons;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Proposal {
+	private List<Flight> route = new ArrayList<Flight>();
+	
+	public double getPrice() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public void setPrice(double v_price) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public List<Flight> getRoute() {
+		// TODO Auto-generated method stub
+		return route;
+	}
+
+}
