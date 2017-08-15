362ca3da3e45df7ddb90b4e66ba07a0ce1d1c327
ivopdm <ivopdm@gmail.com>
2017-08-15 10:22:38 -0300

    Integrado o que estava na classe aviao.

--- IVHM/src/commons/Aircraft.java
+++ IVHM/src/commons/Aircraft.java
@@ -1,12 +1,59 @@
 package commons;
 
+import java.util.List;
+
 /**
  * 
  * @author ivopdm
  * TODO Implementar classe aircraft
  */
 public class Aircraft {
+	private Long id;
+	private String nome;
+	private Double fator;
+	private Double price;
+	private List<Flight> route;
 
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public Double getFator() {
+		return fator;
+	}
+
+	public void setFator(Double fator) {
+		this.fator = fator;
+	}
+
+	public Double getPrice() {
+		return price;
+	}
+
+	public void setPrice(Double price) {
+		this.price = price;
+	}
+
+	public List<Flight> getRoute() {
+		return route;
+	}
+
+	public void setRoute(List<Flight> route) {
+		this.route = route;
+	}
+	
 	public Object getCurrLoc() {
 		// TODO Auto-generated method stub
 		return null;
@@ -15,11 +62,6 @@ public class Aircraft {
 	public void setRoute(Object route) {
 		// TODO Auto-generated method stub
 		
-	}
-
-	public double getPrice() {
-		// TODO Auto-generated method stub
-		return 0;
 	}
 
 	public void setPrice(double v_price) {
